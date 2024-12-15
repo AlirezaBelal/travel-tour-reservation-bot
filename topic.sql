107e51fd74a1e4bbed6bab501bbe4749dd158489
AlirezaBelal <belal.alireza@yahoo.com>
2024-12-15 21:04:34 +0330

    Optimize database schema: Enhanced Data and Topics table structure, added comments and indexing for better performance

--- topic.sql
+++ topic.sql
@@ -1,73 +1,32 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Oct 21, 2023 at 05:46 AM
--- Server version: 10.3.37-MariaDB-cll-lve
--- PHP Version: 8.1.16
+-- Optimized SQL Schema for Topic Management
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Table: Data
+CREATE TABLE `Data`
+(
+    `UserId`   BIGINT(20)                       NOT NULL COMMENT 'Unique identifier for the user (e.g., Telegram user ID)',
+    `Name`     VARCHAR(100)                     NOT NULL COMMENT 'Full name of the user',
+    `Mobile`   VARCHAR(20)                      NOT NULL COMMENT 'User mobile number',
+    `N_Code`   VARCHAR(20)                      NOT NULL COMMENT 'National ID or identifier',
+    `birthday` DATE                             NOT NULL COMMENT 'Date of birth',
+    `sex`      ENUM ('Male', 'Female', 'Other') NOT NULL COMMENT 'Gender of the user',
+    `profile`  VARCHAR(255) DEFAULT NULL COMMENT 'Profile picture URL or path',
+    `step`     VARCHAR(50)                      NOT NULL COMMENT 'Current step in the user workflow',
+    PRIMARY KEY (`UserId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
+-- Table: Topics
+CREATE TABLE `Topics`
+(
+    `id`      BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT 'Unique topic identifier',
+    `Name`    VARCHAR(300) NOT NULL COMMENT 'Topic name',
+    `Groups`  TEXT         NOT NULL COMMENT 'Groups related to the topic',
+    `caption` TEXT         NOT NULL COMMENT 'Detailed description or caption of the topic',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `vfvsdvco_Topic`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Data`
---
-
-CREATE TABLE `Data` (
-  `UserId` bigint(20) NOT NULL,
-  `Name` varchar(100) NOT NULL,
-  `Mobile` varchar(20) NOT NULL,
-  `N_Code` varchar(20) NOT NULL,
-  `birthday` varchar(50) NOT NULL,
-  `sex` varchar(20) NOT NULL,
-  `profile` varchar(20) NOT NULL,
-  `step` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Topics`
---
-
-CREATE TABLE `Topics` (
-  `Name` varchar(300) NOT NULL,
-  `id` bigint(20) NOT NULL,
-  `Groups` varchar(1000) NOT NULL,
-  `caption` varchar(3000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Data`
---
-ALTER TABLE `Data`
-  ADD PRIMARY KEY (`UserId`);
-
---
--- Indexes for table `Topics`
---
-ALTER TABLE `Topics`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Index for faster lookups
+CREATE INDEX idx_name ON `Topics` (`Name`);
