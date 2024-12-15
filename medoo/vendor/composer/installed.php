7527050755bf6d39db8ab82b16b4c491334ebcad
AlirezaBelal <belal.alireza@yahoo.com>
2024-12-15 21:38:27 +0330

    add package

---
+++ medoo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5e81949a84258fa3a5b71ab3a3f9be0fad394e36',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5e81949a84258fa3a5b71ab3a3f9be0fad394e36',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'catfan/medoo' => array(
+            'pretty_version' => 'v2.1.10',
+            'version' => '2.1.10.0',
+            'reference' => 'eb1ad19d127bd358b9a44abc60166b4a2c19a988',
+            'type' => 'framework',
+            'install_path' => __DIR__ . '/../catfan/medoo',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
